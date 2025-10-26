eb543574b56c2bac02ce303e1377c20c627f04a2
sifatahammed <sifatahammed821@gamail.com>
2025-10-26 16:33:49 +0600

    Testing Tailwind CSS and shadcn/ui

--- project-frontend/src/App.jsx
+++ project-frontend/src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+       
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <h1 class="bg-green-300 text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <Button>Click me</Button>
+      </div>
     </>
   )
 }
