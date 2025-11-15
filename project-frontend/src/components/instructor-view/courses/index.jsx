a1c49ceed749e11054b6668b1f2abecafdfbdfb0
sifatahammed <sifatahammed821@gamail.com>
2025-11-15 20:53:21 +0600

    Add Admin Courses Page

---
+++ project-frontend/src/components/instructor-view/courses/index.jsx
@@ -0,0 +1,66 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Delete, Edit } from "lucide-react";
+import { useContext } from "react";
+
+function InstructorCourses() {
+
+  return (
+    <Card>
+      <CardHeader className="flex justify-between flex-row items-center">
+        <CardTitle className="text-3xl font-extrabold">All Courses</CardTitle>
+        <Button className="p-6"
+        >
+          Create New Course
+        </Button>
+      </CardHeader>
+      <CardContent>
+        <div className="overflow-x-auto">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Course</TableHead>
+                <TableHead>Students</TableHead>
+                <TableHead>Revenue</TableHead>
+                <TableHead className="text-right">Actions</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {listOfCourses && listOfCourses.length > 0
+                ? listOfCourses.map((course) => (
+                    <TableRow>
+                      <TableCell className="font-medium">
+                        React
+                      </TableCell>
+                      <TableCell>10</TableCell>
+                      <TableCell>
+                        500
+                      </TableCell>
+                      <TableCell className="text-right">
+                        <Button>
+                          <Edit className="h-6 w-6" />
+                        </Button>
+                        <Button variant="ghost" size="sm">
+                          <Delete className="h-6 w-6" />
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))
+                : null}
+            </TableBody>
+          </Table>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default InstructorCourses;
