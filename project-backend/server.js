af985f8d36dd1544960318712829a56453650417
sifatahammed <sifatahammed821@gamail.com>
2025-11-15 16:06:41 +0600

    Add and Connect Auth-Routes

--- project-backend/server.js
+++ project-backend/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth-routes/index.js");
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -23,6 +25,7 @@ mongoose
   .then(() => console.log("mongodb is connected"))
   .catch((e) => console.log(e));
 
+app.use("/auth", authRoutes);
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
