f1b6b4272a6a4e29d5d646abdbd8d12c3f49e449
sifatahammed <sifatahammed821@gamail.com>
2025-11-15 08:09:10 +0600

    Add AuthPage

--- project-frontend/src/App.jsx
+++ project-frontend/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
+import { Route, Routes } from "react-router-dom";
+import AuthPage from "./pages/auth";
+import RouteGuard from "./components/route-guard";
 import './App.css'
 import { Button } from "@/components/ui/button"
 
 function App() {
   return (
     <>      
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <h1 class="bg-green-300 text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <Button>Click</Button>
-      </div>
+      <Routes>
+      <Route
+        path="/auth"
+        elementelement={<AuthPage />}
+      />
+      </Routes>
     </>
   )
 }
