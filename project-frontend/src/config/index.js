30f44fc49b7a6d4802308b9777d53ab60949b2b9
sifatahammed <sifatahammed821@gamail.com>
2025-11-15 14:58:12 +0600

    Add Required Components/ui

---
+++ project-frontend/src/config/index.js
@@ -0,0 +1,40 @@
+export const signUpFormControls = [
+  {
+    name: "userName",
+    label: "User Name",
+    placeholder: "Enter your user name",
+    type: "text",
+    componentType: "input",
+  },
+  {
+    name: "userEmail",
+    label: "User Email",
+    placeholder: "Enter your user email",
+    type: "email",
+    componentType: "input",
+  },
+  {
+    name: "password",
+    label: "Password",
+    placeholder: "Enter your password",
+    type: "password",
+    componentType: "input",
+  },
+];
+
+export const signInFormControls = [
+  {
+    name: "userEmail",
+    label: "User Email",
+    placeholder: "Enter your user email",
+    type: "email",
+    componentType: "input",
+  },
+  {
+    name: "password",
+    label: "Password",
+    placeholder: "Enter your password",
+    type: "password",
+    componentType: "input",
+  },
+];
