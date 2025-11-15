b6f03af870556661aa1644f2abeb3a103d00f640
sifatahammed <sifatahammed821@gamail.com>
2025-11-15 16:19:41 +0600

    Fetch Axios data to Auth-Context

---
+++ project-frontend/services/index.js
@@ -0,0 +1,16 @@
+import axiosInstance from "@/api/axiosInstance";
+
+export async function registerService(formData) {
+  const { data } = await axiosInstance.post("/auth/register", {
+    ...formData,
+    role: "user",
+  });
+
+  return data;
+}
+
+export async function loginService(formData) {
+  const { data } = await axiosInstance.post("/auth/login", formData);
+
+  return data;
+}
--- project-frontend/src/context/auth-context/index.jsx
+++ project-frontend/src/context/auth-context/index.jsx
@@ -1,10 +1,15 @@
 import { createContext, useEffect, useState } from "react";
+import { registerService } from "@/services";
 
 export const AuthContext = createContext(null);
 
 export default function AuthProvider({ children }) {
   const [signInFormData, setSignInFormData] = useState(initialSignInFormData);
   const [signUpFormData, setSignUpFormData] = useState(initialSignUpFormData);
+  async function handleRegisterUser(event) {
+    event.preventDefault();
+    const data = await registerService(signUpFormData);
+  }
   return (
     <AuthContext.Provider
       value={{
@@ -12,6 +17,7 @@ export default function AuthProvider({ children }) {
         setSignInFormData,
         signUpFormData,
         setSignUpFormData,
+        handleRegisterUser,
       }}
     >
       { children}
