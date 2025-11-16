47e0236cc8defce22277d0b21b70af3fde570796
sifatahammed <sifatahammed821@gamail.com>
2025-11-16 16:26:05 +0600

    Add Form Validator-New Course-Instructor

--- project-frontend/src/pages/instructor/add-new-course.jsx
+++ project-frontend/src/pages/instructor/add-new-course.jsx
@@ -4,41 +4,86 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import CourseCurriculum from "@/components/instructor-view/courses/add-new-course/course-curriculum";
 import CourseLanding from "@/components/instructor-view/courses/add-new-course/course-landing";
 import CourseSettings from "@/components/instructor-view/courses/add-new-course/course-settings";
+import { InstructorContext } from "@/context/instructor-context";
+
 
 function AddNewCoursePage() {
+   const {
+    courseLandingFormData,
+    courseCurriculumFormData,
+    setCourseLandingFormData,
+    setCourseCurriculumFormData,
+    currentEditedCourseId,
+    setCurrentEditedCourseId,
+  } = useContext(InstructorContext);
+
+  function isEmpty(value) {
+    if (Array.isArray(value)) {
+      return value.length === 0;
+    }
+
+    return value === "" || value === null || value === undefined;
+  }
+
+  function validateFormData() {
+    for (const key in courseLandingFormData) {
+      if (isEmpty(courseLandingFormData[key])) {
+        return false;
+      }
+    }
+
+    let hasFreePreview = false;
+
+    for (const item of courseCurriculumFormData) {
+      if (
+        isEmpty(item.title) ||
+        isEmpty(item.videoUrl) ||
+        isEmpty(item.public_id)
+      ) {
+        return false;
+      }
+
+      if (item.freePreview) {
+        hasFreePreview = true; //found at least one free preview
+      }
+    }
+
+    return hasFreePreview;
+  }
+
     return (
-        <div className="container mx-auto p-4">
-      <div className="flex justify-between">
-        <h1 className="text-3xl font-extrabold mb-5">Create a new course</h1>
-        <Button>
-          SUBMIT
-        </Button>
+      <div className="container mx-auto p-4">
+        <div className="flex justify-between">
+          <h1 className="text-3xl font-extrabold mb-5">Create a new course</h1>
+          <Button>
+            SUBMIT
+          </Button>
+        </div>
+        <Card>
+          <CardContent>
+            <div className="container mx-auto p-4">
+              <Tabs defaultValue="curriculum" className="space-y-4">
+                <TabsList>
+                  <TabsTrigger value="curriculum">Curriculum</TabsTrigger>
+                  <TabsTrigger value="course-landing-page">
+                    Course Landing Page
+                  </TabsTrigger>
+                  <TabsTrigger value="settings">Settings</TabsTrigger>
+                </TabsList>
+                <TabsContent value="curriculum">
+                  <CourseCurriculum />
+                </TabsContent>
+                <TabsContent value="course-landing-page">
+                  <CourseLanding />
+                </TabsContent>
+                <TabsContent value="settings">
+                  <CourseSettings />
+                </TabsContent>
+              </Tabs>
+            </div>
+          </CardContent>
+        </Card>
       </div>
-      <Card>
-        <CardContent>
-          <div className="container mx-auto p-4">
-            <Tabs defaultValue="curriculum" className="space-y-4">
-              <TabsList>
-                <TabsTrigger value="curriculum">Curriculum</TabsTrigger>
-                <TabsTrigger value="course-landing-page">
-                  Course Landing Page
-                </TabsTrigger>
-                <TabsTrigger value="settings">Settings</TabsTrigger>
-              </TabsList>
-              <TabsContent value="curriculum">
-                <CourseCurriculum />
-              </TabsContent>
-              <TabsContent value="course-landing-page">
-                <CourseLanding />
-              </TabsContent>
-              <TabsContent value="settings">
-                <CourseSettings />
-              </TabsContent>
-            </Tabs>
-          </div>
-        </CardContent>
-      </Card>
-    </div>
   );
 }
 
