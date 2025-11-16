fa0c761c6fc73fc2593a2eb58e09b6f85d8d8e93
sifatahammed <sifatahammed821@gamail.com>
2025-11-16 19:21:39 +0600

    Update Add-New-Course Page-Final

--- project-frontend/src/pages/instructor/add-new-course.jsx
+++ project-frontend/src/pages/instructor/add-new-course.jsx
@@ -1,27 +1,32 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import CourseCurriculum from "@/components/instructor-view/courses/add-new-course/course-curriculum";
 import CourseLanding from "@/components/instructor-view/courses/add-new-course/course-landing";
 import CourseSettings from "@/components/instructor-view/courses/add-new-course/course-settings";
-import { InstructorContext } from "@/context/instructor-context";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { courseCurriculumInitialFormData,courseLandingInitialFormData } from "@/config";
 import { AuthContext } from "@/context/auth-context";
+import { InstructorContext } from "@/context/instructor-context";
+import {
+  addNewCourseService, fetchInstructorCourseDetailsService, updateCourseByIdService,
+} from "@/services";
+import { useContext, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-
 function AddNewCoursePage() {
-   const {
+  const {
     courseLandingFormData,
     courseCurriculumFormData,
     setCourseLandingFormData,
     setCourseCurriculumFormData,
     currentEditedCourseId,
-    setCurrentEditedCourseId,
-  } = useContext(InstructorContext);
+    setCurrentEditedCourseId } = useContext(InstructorContext);
 
   const { auth } = useContext(AuthContext);
   const navigate = useNavigate();
   const params = useParams();
+
+  console.log(params);
 
   function isEmpty(value) {
     if (Array.isArray(value)) {
@@ -118,39 +123,43 @@ function AddNewCoursePage() {
 
   console.log(params, currentEditedCourseId, "params");
 
-    return (
-      <div className="container mx-auto p-4">
-        <div className="flex justify-between">
-          <h1 className="text-3xl font-extrabold mb-5">Create a new course</h1>
-          <Button>
-            SUBMIT
-          </Button>
-        </div>
-        <Card>
-          <CardContent>
-            <div className="container mx-auto p-4">
-              <Tabs defaultValue="curriculum" className="space-y-4">
-                <TabsList>
-                  <TabsTrigger value="curriculum">Curriculum</TabsTrigger>
-                  <TabsTrigger value="course-landing-page">
-                    Course Landing Page
-                  </TabsTrigger>
-                  <TabsTrigger value="settings">Settings</TabsTrigger>
-                </TabsList>
-                <TabsContent value="curriculum">
-                  <CourseCurriculum />
-                </TabsContent>
-                <TabsContent value="course-landing-page">
-                  <CourseLanding />
-                </TabsContent>
-                <TabsContent value="settings">
-                  <CourseSettings />
-                </TabsContent>
-              </Tabs>
-            </div>
-          </CardContent>
-        </Card>
+  return (
+    <div className="container mx-auto p-4">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-extrabold mb-5">Create a new course</h1>
+        <Button
+          disabled={!validateFormData()}
+          className="text-sm tracking-wider font-bold px-8"
+          onClick={handleCreateCourse}
+        >
+          SUBMIT
+        </Button>
       </div>
+      <Card>
+        <CardContent>
+          <div className="container mx-auto p-4">
+            <Tabs defaultValue="curriculum" className="space-y-4">
+              <TabsList>
+                <TabsTrigger value="curriculum">Curriculum</TabsTrigger>
+                <TabsTrigger value="course-landing-page">
+                  Course Landing Page
+                </TabsTrigger>
+                <TabsTrigger value="settings">Settings</TabsTrigger>
+              </TabsList>
+              <TabsContent value="curriculum">
+                <CourseCurriculum />
+              </TabsContent>
+              <TabsContent value="course-landing-page">
+                <CourseLanding />
+              </TabsContent>
+              <TabsContent value="settings">
+                <CourseSettings />
+              </TabsContent>
+            </Tabs>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
