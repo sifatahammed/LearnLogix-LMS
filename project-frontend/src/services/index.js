134de199c8b5ea047960ada21cb170d7159600a5
sifatahammed <sifatahammed821@gamail.com>
2025-11-16 18:44:13 +0600

    Add Course Axios Calls-Instructor

--- project-frontend/src/services/index.js
+++ project-frontend/src/services/index.js
@@ -52,3 +52,32 @@ export async function mediaBulkUploadService(formData, onProgressCallback) {
 
   return data;
 }
+
+export async function fetchInstructorCourseListService() {
+  const { data } = await axiosInstance.get(`/instructor/course/get`);
+
+  return data;
+}
+
+export async function addNewCourseService(formData) {
+  const { data } = await axiosInstance.post(`/instructor/course/add`, formData);
+
+  return data;
+}
+
+export async function fetchInstructorCourseDetailsService(id) {
+  const { data } = await axiosInstance.get(
+    `/instructor/course/get/details/${id}`
+  );
+
+  return data;
+}
+
+export async function updateCourseByIdService(id, formData) {
+  const { data } = await axiosInstance.put(
+    `/instructor/course/update/${id}`,
+    formData
+  );
+
+  return data;
+}
