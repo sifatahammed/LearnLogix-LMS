2d72a491f5cdc977acd3bdf90839bac71b4f70c2
sifatahammed <sifatahammed821@gamail.com>
2025-11-16 08:03:57 +0600

    Add Course Landing Page

--- project-frontend/src/main.jsx
+++ project-frontend/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AuthProvider from "./context/auth-context/index.jsx";
+import { InstructorContext } from "./context/instructor-context";
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <InstructorContext>
+        <App/>
+      </InstructorContext>
     </AuthProvider>
   </BrowserRouter>
 )
