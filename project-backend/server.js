ea44fef6461914ccb634afad3fd3f7ab406afb31
sifatahammed <sifatahammed821@gamail.com>
2025-11-19 15:45:36 +0600

    Add and Connect Order-routes

--- project-backend/server.js
+++ project-backend/server.js
@@ -6,6 +6,7 @@ const authRoutes = require("./routes/auth-routes/index.js");
 const mediaRoutes = require("./routes/instructor-routes/media-routes");
 const instructorCourseRoutes = require("./routes/instructor-routes/course-routes");
 const studentViewCourseRoutes = require("./routes/student-routes/course-routes");
+const studentViewOrderRoutes = require("./routes/student-routes/order-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -31,6 +32,7 @@ app.use("/auth", authRoutes);
 app.use("/media", mediaRoutes);
 app.use("/instructor/course", instructorCourseRoutes);
 app.use("/student/course", studentViewCourseRoutes);
+app.use("/student/order", studentViewOrderRoutes);
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
