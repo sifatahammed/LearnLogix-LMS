e5c4c05255b28a6ba2be50d9d82ecbd4fae8811f
sifatahammed <sifatahammed821@gamail.com>
2025-11-20 14:49:43 +0600

    Add Payment Return Page

--- project-frontend/src/App.jsx
+++ project-frontend/src/App.jsx
@@ -11,6 +11,7 @@ import StudentHomePage from "./pages/student/home";
 import NotFoundPage from "./pages/not-found";
 import StudentViewCoursesPage from "./pages/student/courses";
 import StudentViewCourseDetailsPage from "./pages/student/course-details";
+import PaypalPaymentReturnPage from "./pages/student/payment-return";
 
 function App() {
   const { auth } = useContext(AuthContext);
@@ -74,7 +75,7 @@ function App() {
       <Route path="" element={<StudentHomePage />} />
       <Route path="home" element={<StudentHomePage />} />
       <Route path="courses" element={<StudentViewCoursesPage />} />
-
+      <Route path="payment-return" element={<PaypalPaymentReturnPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
