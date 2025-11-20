640c4b21a452bbcb51418667ab98db529d2db82b
sifatahammed <sifatahammed821@gamail.com>
2025-11-20 21:45:26 +0600

    Add and Connect Student Course Progress Routes

--- project-backend/server.js
+++ project-backend/server.js
@@ -8,6 +8,7 @@ const instructorCourseRoutes = require("./routes/instructor-routes/course-routes
 const studentViewCourseRoutes = require("./routes/student-routes/course-routes");
 const studentViewOrderRoutes = require("./routes/student-routes/order-routes");
 const studentCoursesRoutes = require("./routes/student-routes/student-courses-routes");
+const studentCourseProgressRoutes = require("./routes/student-routes/course-progress-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -35,6 +36,7 @@ app.use("/instructor/course", instructorCourseRoutes);
 app.use("/student/course", studentViewCourseRoutes);
 app.use("/student/order", studentViewOrderRoutes);
 app.use("/student/courses-bought", studentCoursesRoutes);
+app.use("/student/course-progress", studentCourseProgressRoutes);
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
