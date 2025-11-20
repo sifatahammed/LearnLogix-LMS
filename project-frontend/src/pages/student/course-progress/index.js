9cbca2d77f7e02333cfc4aa008b77c76e0aec5eb
sifatahammed <sifatahammed821@gamail.com>
2025-11-20 21:41:19 +0600

    Add Student Course Progress Page

--- project-frontend/src/pages/student/course-progress/index.js
+++ project-frontend/src/pages/student/course-progress/index.js
@@ -1,7 +1,127 @@
-function StudentViewCourseProgressPage() {
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import VideoPlayer from "@/components/video-player";
+import { Check, ChevronLeft, ChevronRight, Play } from "lucide-react";
+import Confetti from "react-confetti";
 
+function StudentViewCourseProgressPage() {
+ 
   return (
-    <div>
+    <div className="flex flex-col h-screen bg-[#1c1d1f] text-white">
+      {showConfetti && <Confetti />}
+      <div className="flex items-center justify-between p-4 bg-[#1c1d1f] border-b border-gray-700">
+        <div className="flex items-center space-x-4">
+          <Button
+            onClick={() => navigate("/student-courses")}
+            className="text-black"
+            variant="ghost"
+            size="sm"
+          >
+            <ChevronLeft className="h-4 w-4 mr-2" />
+            Back to My Courses Page
+          </Button>
+          <h1 className="text-lg font-bold hidden md:block">
+            title
+          </h1>
+        </div>
+        <Button onClick={() => setIsSideBarOpen(!isSideBarOpen)}>
+          {isSideBarOpen ? (
+            <ChevronRight className="h-5 w-5" />
+          ) : (
+            <ChevronLeft className="h-5 w-5" />
+          )}
+        </Button>
+      </div>
+      <div className="flex flex-1 overflow-hidden">
+        <div
+          className={`flex-1 ${
+            isSideBarOpen ? "mr-[400px]" : ""
+          } transition-all duration-300`}
+        >
+          <VideoPlayer
+            width="100%"
+            height="500px"
+    
+          />
+          <div className="p-6 bg-[#1c1d1f]">
+            <h2 className="text-2xl font-bold mb-2">title</h2>
+          </div>
+        </div>
+        <div
+          className={`fixed top-[64px] right-0 bottom-0 w-[400px] bg-[#1c1d1f] border-l border-gray-700 transition-all duration-300 ${
+            isSideBarOpen ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <Tabs defaultValue="content" className="h-full flex flex-col">
+            <TabsList className="grid bg-[#1c1d1f] w-full grid-cols-2 p-0 h-14">
+              <TabsTrigger
+                value="content"
+                className=" text-black rounded-none h-full"
+              >
+                Course Content
+              </TabsTrigger>
+              <TabsTrigger
+                value="overview"
+                className=" text-black rounded-none h-full"
+              >
+                Overview
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="content">
+              <ScrollArea className="h-full">
+                <div className="p-4 space-y-4">
+            
+                </div>
+              </ScrollArea>
+            </TabsContent>
+            <TabsContent value="overview" className="flex-1 overflow-hidden">
+              <ScrollArea className="h-full">
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-4">About this course</h2>
+                  <p className="text-gray-400">
+                    description
+                  </p>
+                </div>
+              </ScrollArea>
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+      <Dialog open={lockCourse}>
+        <DialogContent className="sm:w-[425px]">
+          <DialogHeader>
+            <DialogTitle>You can't view this page</DialogTitle>
+            <DialogDescription>
+              Please purchase this course to get access
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={showCourseCompleteDialog}>
+        <DialogContent showOverlay={false} className="sm:w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Congratulations!</DialogTitle>
+            <DialogDescription className="flex flex-col gap-3">
+              <Label>You have completed the course</Label>
+              <div className="flex flex-row gap-3">
+                <Button onClick={() => navigate("/student-courses")}>
+                  My Courses Page
+                </Button>
+                <Button>Rewatch Course</Button>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
