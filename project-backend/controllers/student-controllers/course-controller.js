dd536aa25a31b5336bf6b9fc5c0b0dd0bcd77933
sifatahammed <sifatahammed821@gamail.com>
2025-11-20 15:32:27 +0600

    Update and Connect Course-Controller-Student

--- project-backend/controllers/student-controllers/course-controller.js
+++ project-backend/controllers/student-controllers/course-controller.js
@@ -1,5 +1,6 @@
-const Course = require("../../models/Course");
-const StudentCourses = require("../../models/StudentCourses");
+const Course = require("../../models/Course.js");
+
+const StudentCourses = require("../../models/StudentCourses.js");
 
 const getAllStudentViewCourses = async (req, res) => {
   try {
@@ -91,18 +92,19 @@ const getStudentViewCourseDetails = async (req, res) => {
 const checkCoursePurchaseInfo = async (req, res) => {
   try {
     const { id, studentId } = req.params;
-    const studentCourses = await StudentCourses.findOne({
-      userId: studentId,
-    });
+    const studentCourses = await StudentCourses.findOne({ userId: studentId, });
 
-    const ifStudentAlreadyBoughtCurrentCourse =
-      studentCourses.courses.findIndex((item) => item.courseId === id) > -1;
+    const ifStudentAlreadyBoughtCurrentCourse = studentCourses.courses.findIndex(
+      (item) => item.courseId === id) > -1;
+
     res.status(200).json({
       success: true,
       data: ifStudentAlreadyBoughtCurrentCourse,
     });
-  } catch (e) {
+  } 
+  catch (e) {
     console.log(e);
+
     res.status(500).json({
       success: false,
       message: "Some error occured!",
@@ -111,7 +113,5 @@ const checkCoursePurchaseInfo = async (req, res) => {
 };
 
 module.exports = {
-  getAllStudentViewCourses,
-  getStudentViewCourseDetails,
-  checkCoursePurchaseInfo,
+  getAllStudentViewCourses, checkCoursePurchaseInfo, getStudentViewCourseDetails,
 };
--- project-backend/routes/student-routes/course-routes.js
+++ project-backend/routes/student-routes/course-routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const {
-  getAllStudentViewCourses, getStudentViewCourseDetails
+  getAllStudentViewCourses, getStudentViewCourseDetails, checkCoursePurchaseInfo
 } = require("../../controllers/student-controller/course-controller");
 const router = express.Router();
 
 router.get("/get", getAllStudentViewCourses);
 router.get("/get/details/:id", getStudentViewCourseDetails);
+router.get("/purchase-info/:id/:studentId", checkCoursePurchaseInfo);
 
 module.exports = router;
